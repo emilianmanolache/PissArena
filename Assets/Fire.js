b04f8f3869469171cf37ce25eac7df154661b649
Emilian Manolache <e.manolache@dataclaritycorp.com>
2015-11-07 01:54:35 +0200

    First version on Git

---
+++ Assets/Fire.js
@@ -0,0 +1,16 @@
+﻿#pragma strict
+
+function Start () {
+
+}
+
+function Update () {
+    var fluvio = GetComponent(Thinksquirrel.Fluvio.FluidParticleSystem);
+    var pissSim = fluvio.GetParticleSystem();
+    if(Input.GetButtonDown("Fire1") && pissSim.isStopped) {
+        pissSim.Play();
+    }
+    if(!Input.GetButton("Fire1") && pissSim.isPlaying) {
+        pissSim.Stop();
+    }
+}
